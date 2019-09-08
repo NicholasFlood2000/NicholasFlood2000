d12670caec7949dd12409b0063f112f9eee63492
NicholasFlood2000 <55025838+NicholasFlood2000@users.noreply.github.com>
2019-09-07 22:14:17 -0400

    RPS WIN COUNTER

--- js/RockPaperScissors.js
+++ js/RockPaperScissors.js
@@ -1,57 +1,75 @@
-$( document ).ready(function() {
-
+var wins = 0;
+var losses = 0;
+var ties = 0;
 $(".choice").click(function() {
-    var user;
-    var computer;
-    user = this.id;
-    computer = Math.random();
-    if (computer <= 1 / 3) {
-      computer = "rock";
-      document.getElementById("cpu").innerHTML = "rock";
-      if (user == "rock") {
-        document.getElementById("result").innerHTML = "TIE";
-        document.getElementById("you").innerHTML = "rock";
-      } else if (user == "paper") {
-        document.getElementById("result").innerHTML = "WIN";
-        document.getElementById("you").innerHTML = "paper";
-      } else {
-        document.getElementById("result").innerHTML = "LOSE";
-        document.getElementById("you").innerHTML = "scissors";
-      }
-      console.log("Rock");
-      console.log(user);
-    } else if (computer < 2 / 3) {
-      computer = "scissors";
-      document.getElementById("cpu").innerHTML = "scissors";
-      if (user == "scissors") {
-        document.getElementById("result").innerHTML = "TIE";
-        document.getElementById("you").innerHTML = "scissors";
-      } else if (user == "rock") {
-       document.getElementById("result").innerHTML = "WIN";
-        document.getElementById("you").innerHTML = "rock";
-      } else {
-        document.getElementById("result").innerHTML = "LOSE";
-        document.getElementById("you").innerHTML = "paper";
+  var user;
+  var computer;
+  user = this.id;
+  computer = Math.random();
+  if (computer <= 1 / 3) {
+    computer = "rock";
+    document.getElementById("cpu").innerHTML = "rock";
+    if (user == "rock") {
+      document.getElementById("result").innerHTML = "You Tied";
+      document.getElementById("you").innerHTML = "rock";
+      var t = ties++;
+      document.getElementById("ties").innerHTML = t;
+    } else if (user == "paper") {
+      document.getElementById("result").innerHTML = "You Won";
+      document.getElementById("you").innerHTML = "paper";
+      var w = wins++;
+      document.getElementById("wins").innerHTML = w;
+    } else {
+      document.getElementById("result").innerHTML = "You Lost";
+      document.getElementById("you").innerHTML = "scissors";
+      var l = losses++;
+      document.getElementById("losses").innerHTML = l;
+    }
+    console.log("Rock");
+    console.log(user);
+  } else if (computer < 2 / 3) {
+    computer = "scissors";
+    document.getElementById("cpu").innerHTML = "scissors";
+    if (user == "scissors") {
+      document.getElementById("result").innerHTML = "You Tied";
+      document.getElementById("you").innerHTML = "scissors";
+      var t = ties++;
+      document.getElementById("ties").innerHTML = t;
+    } else if (user == "rock") {
+      document.getElementById("result").innerHTML = "You Won";
+      document.getElementById("you").innerHTML = "rock";
+      var w = wins++;
+      document.getElementById("wins").innerHTML = w;
+    } else {
+      document.getElementById("result").innerHTML = "You Lost";
+      document.getElementById("you").innerHTML = "paper";
+      var l = losses++;
+      document.getElementById("losses").innerHTML = l;
       console.log("Scissors");
       console.log(user);
-      }
-    } else {
-      computer = "paper";
-      document.getElementById("cpu").innerHTML = "paper";
-      if (user == "paper") {
-        document.getElementById("result").innerHTML = "TIE";
-        document.getElementById("you").innerHTML = "paper";
-      } else if (user == "scissors") {
-       document.getElementById("result").innerHTML = "WIN";
-       document.getElementById("you").innerHTML = "scissors";
-      } else {
-        document.getElementById("result").innerHTML = "LOSE";
-        document.getElementById("you").innerHTML = "rock";
-      }
-      console.log("Paper");
-      console.log(user);
     }
-    var winner = compare(user, computer);
-    console.log(winner);
-  });
+  } else {
+    computer = "paper";
+    document.getElementById("cpu").innerHTML = "paper";
+    if (user == "paper") {
+      document.getElementById("result").innerHTML = "You Tied";
+      document.getElementById("you").innerHTML = "paper";
+      var t = ties++;
+      document.getElementById("ties").innerHTML = t;
+    } else if (user == "scissors") {
+      document.getElementById("result").innerHTML = "You Won";
+      document.getElementById("you").innerHTML = "scissors";
+      var w = wins++;
+      document.getElementById("wins").innerHTML = w;
+    } else {
+      document.getElementById("result").innerHTML = "You Lost";
+      document.getElementById("you").innerHTML = "rock";
+      var l = losses++;
+      document.getElementById("losses").innerHTML = l;
+    }
+    console.log("Paper");
+    console.log(user);
+  }
+  var winner = compare(user, computer);
+  console.log(winner);
 });
